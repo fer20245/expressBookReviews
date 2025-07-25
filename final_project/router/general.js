a9a97d958577a520971c9e3055ae04ff076e94c8
fer20245 <ricardoreyes20232024@gmail.com>
2025-07-25 16:51:32 -0400

    tarea2 realizada con exito del proyecto final curso nodejs

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,23 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    
+  res.send(books);
+  //return res.status(300).json({message: "Yet to be implemented"});
+  });
+  
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+  
+    // Buscar el libro por la llave ISBN
+    if (books[isbn]) {
+      return res.status(200).json(books[isbn]);
+    } else {
+      return res.status(404).json({ mensaje: 'Libro no encontrado con ese ISBN' });
+    }
+  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
